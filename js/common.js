8497fecdada1fd0dbf5dc6ab6b3b26661b7cea0d
Valentin Lipai <valentine.webdev@gmail.com>
2018-04-26 23:17:35 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -32,21 +32,32 @@ $(function(){
 	function toplineStyleControl(top){
 		if ( top > scrollrange && topline.hasClass('topline__scrolling') == false) 
 		{
-				topline.addClass('topline__scrolling');
+			topline.addClass('topline__scrolling');
 		} 
 		else if (top <= scrollrange && topline.hasClass('topline__scrolling') == true) 
 		{
-				topline.removeClass('topline__scrolling');
+			topline.removeClass('topline__scrolling');
 		}
 	};
 
 	function startVideo(){
 		if (video.hasClass('playing') == false) 
 		{
-				video.addClass('playing');
-				video.get(0).play();
+			video.addClass('playing');
+			video.get(0).play();
 		}
 	}
+
+
+	var player = document.getElementById('videoSection-video');
+
+	player.onended = function(){
+		player.load();
+	}
+	
+
+
+
 
 	$(window).on('scroll', function(){
 		var fromTop   = $(window).scrollTop();
@@ -67,30 +78,30 @@ $(function(){
 $(function(){
 // header slider start
 
-		var headerSliderWrapper   = $('#header-slider-wrapper');
-		var switchTime = 2500;
-		var changeTime = switchTime * 2;
+var headerSliderWrapper   = $('#header-slider-wrapper');
+var switchTime = 2500;
+var changeTime = switchTime * 2;
 
-		function showNextSlide(){
+function showNextSlide(){
 
-			var currsentSlide = $('.header-bg__img.active');
+	var currsentSlide = $('.header-bg__img.active');
 
-			if ( headerSliderWrapper.find('.header-bg__img:last').hasClass('active') ){
-				currsentSlide.fadeOut(switchTime);
-				currsentSlide.removeClass('active');
-				headerSliderWrapper.find('.header-bg__img:first').addClass('active').fadeIn(switchTime);
-			} else {
-				currsentSlide.fadeOut(switchTime).removeClass('active');
-				currsentSlide.next().addClass('active').fadeIn(switchTime);
-			}
+	if ( headerSliderWrapper.find('.header-bg__img:last').hasClass('active') ){
+		currsentSlide.fadeOut(switchTime);
+		currsentSlide.removeClass('active');
+		headerSliderWrapper.find('.header-bg__img:first').addClass('active').fadeIn(switchTime);
+	} else {
+		currsentSlide.fadeOut(switchTime).removeClass('active');
+		currsentSlide.next().addClass('active').fadeIn(switchTime);
+	}
 
-		}
+}
 
-			function headerBgSlider(){
-					setInterval(showNextSlide, changeTime);
-			};
+function headerBgSlider(){
+	setInterval(showNextSlide, changeTime);
+};
 
-			$(document).ready(function() {
+$(document).ready(function() {
 				// headerSliderWrapper.find('.header-bg__img:first').addClass('active').fadeIn();
 				headerBgSlider();
 			});
@@ -100,44 +111,44 @@ $(function(){
 
 $(function() {
 		// mask tel
-	var telInp = $('input[name="tel"]');
-				
+		var telInp = $('input[name="tel"]');
+
 		telInp.each(function(){
 			$(this).mask("+375 (99) 999-99-99");
 
-				$(this).click(function(){
+			$(this).click(function(){
 				if($(this).val() == '+375 (__) ___-__-__'){
 					$(this).setCursorPosition(6);
 				}
-				});
+			});
 		});	
-			
-	// set cursore position
-		$.fn.setCursorPosition = function(pos) {
-			if ($(this).get(0).setSelectionRange) {
-				$(this).get(0).setSelectionRange(pos, pos);
-			} else if ($(this).get(0).createTextRange) {
-				var range = $(this).get(0).createTextRange();
 
-				range.collapse(true);
-				range.moveEnd('character', pos);
-				range.moveStart('character', pos);
-				range.select();
-			}
-		};
+	// set cursore position
+	$.fn.setCursorPosition = function(pos) {
+		if ($(this).get(0).setSelectionRange) {
+			$(this).get(0).setSelectionRange(pos, pos);
+		} else if ($(this).get(0).createTextRange) {
+			var range = $(this).get(0).createTextRange();
+
+			range.collapse(true);
+			range.moveEnd('character', pos);
+			range.moveStart('character', pos);
+			range.select();
+		}
+	};
 
 
 
 			// popup start
-		var popupForm                 = $('.popup-form-block'),
-				popupTextBlocks           = $('.popup-text-blocks'),
-				popupCloseBtn             = $('.close-popup'),
-				body                      = $('body');
+			var popupForm                 = $('.popup-form-block'),
+			popupTextBlocks           = $('.popup-text-blocks'),
+			popupCloseBtn             = $('.close-popup'),
+			body                      = $('body');
 
 		// узнаем ширину скроллбара
 		var clientWidth      = $(window).innerWidth(),
-				winWidth         = $(window).outerWidth(),
-				scrollBarWidth   = parseFloat( winWidth - clientWidth);
+		winWidth         = $(window).outerWidth(),
+		scrollBarWidth   = parseFloat( winWidth - clientWidth);
 				// body.css('padding-right', scrollBarWidth).css('overflow', 'hidden');  // Пример 
 		// управление модальными окнами
 
@@ -158,7 +169,7 @@ $(function() {
 
 
 		// smooth scroll
-			$("a.scroll-link").on("click", function (event) {
+		$("a.scroll-link").on("click", function (event) {
 				//отменяем стандартную обработку нажатия по ссылке
 				event.preventDefault();
 
@@ -166,7 +177,7 @@ $(function() {
 				var id  = $(this).attr('href'),
 
 				//узнаем высоту от начала страницы до блока на который ссылается якорь
-					top = $(id).offset().top;
+				top = $(id).offset().top;
 				
 				//анимируем переход на расстояние - top за 1500 мс
 				$('body,html').animate({scrollTop: top}, 1500);
@@ -193,4 +204,4 @@ $(function() {
 
 
 
-});
+		});
